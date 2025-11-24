214d353d621f36d832c393760491ef282d5de0ef
mosesdeveloper-cyber <norteymoses769@gmail.com>
2025-11-24 19:37:27 +0100

    Update basicsExercise2.py
    Basics2..

--- 1 - Variables/basicsExercise2.py
+++ 1 - Variables/basicsExercise2.py
@@ -2,9 +2,9 @@
 # Write a program to input two whole numbers,
 # add them together and print the result to the screen. 
 
-def main():
-    number1= int(input("Enter your first number "))
-    number2= int(input("Enter your second number "))
-    sumOfNumbers = number1 + number2
-    print("{0} + {1} = {2}".format(number1,number2,sumOfNumbers))
+numbers():
+    number1= input("5")
+    number2= input("3")
+    sumofnumbers = number1 + number2
+    print("{5} + {3} = {8}". (number1,number2,sumOfNumbers))
     
