f7e7e724e68baf9620eff230584a68e33ce34600
mosesdeveloper-cyber <norteymoses769@gmail.com>
2025-11-24 21:11:45 +0100

    Update additional_exercise4.py
    Leg

--- 1 - Variables/Additional Exercises/additional_exercise4.py
+++ 1 - Variables/Additional Exercises/additional_exercise4.py
@@ -2,16 +2,12 @@
 #additional exercise 4
 #sample solution
 
-("Dividing - improved")
-print("This program asks for two numbers and then divides them,")
-print("it then displays the number of times one goes into the other")
-print()
-num1 = int(input("Please enter a number: "))
-num2 = int(input("Please enter a second number: "))
-print()
-#many ways to do this
-intans = num1 // num2
-#intans = int(num1/num2)
-#intans = int(round(num1/num2,0))
-remainder = num1 % num2
-print("{0}/{1} = {2}.".format(num1,num2,intans,remainder))
+numbers():
+num1 = input("8")
+num2 = input("4")
+intans():
+intans = num1 / num2
+print(" num1 / num2 = intans")
+remainder = num1 / num2
+print (" num1 / num2 = remainder")
+print("{8}/{4} = {2}.".(num1,num2,intans,remainder))
