3caf5b327450887f8edc5d773ec3090241035c3b
mosesdeveloper-cyber <norteymoses769@gmail.com>
2025-11-24 19:30:27 +0100

    Update basicsExercise1.py
    Basics1..

--- 1 - Variables/basicsExercise1.py
+++ 1 - Variables/basicsExercise1.py
@@ -1,9 +1,9 @@
 # Exercise 1 Basics
 # Write a program to input two messages and output them to a user
 
-def main():
-    message1= input("Enter your first message ")
-    message2= input("Enter your second message ")
-    print("Your first message was {0} ".format(message1))
-    print("Your second message was {0} ".format(message2))
-    
+messages():
+    message1= input("I love playing football")
+    message2= input("I love playing on the piano")
+    print("I love playing football".(message1))
+    print("I love playing on the piano".(message2))
+    print("message1+message2") 
