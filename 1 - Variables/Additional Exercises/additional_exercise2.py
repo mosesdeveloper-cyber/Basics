e8fab0dd738d3697b30c70fe93fc6fea90560785
mosesdeveloper-cyber <norteymoses769@gmail.com>
2025-11-24 20:44:05 +0100

    Update additional_exercise2.py
    Number5

--- 1 - Variables/Additional Exercises/additional_exercise2.py
+++ 1 - Variables/Additional Exercises/additional_exercise2.py
@@ -2,13 +2,10 @@
 #additional exercise 2
 #sample solution
 
-print("Number Multiply and Divsion")
-print("This program asks for three numbers, adds the first two together")
-print("then divides the total by the third number before displaying the answer")
-print()
-num1 = int(input("Please enter a number: "))
-num2 = int(input("Please enter a second number: "))
-num3 = int(input("Please enter a third number: "))
-print()
+numbers():
+num1 = input("10")
+num2 = input("8")
+num3 = input("6")
+answer():
 ans = (num1 + num2) / num3
-print("The total of ({0} + {1}) / {2} is {3}.".format(num1,num2,num3,ans))
+print("The total of ({10} + {8}) / {6} is {3}.".(num1,num2,num3,ans))
