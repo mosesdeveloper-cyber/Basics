6896e6960efbef747d2d6923c2016fd4e1f05b9b
mosesdeveloper-cyber <norteymoses769@gmail.com>
2025-11-25 23:50:54 +0100

    Update additional_exercise9.py
    9

--- 1 - Variables/Additional Exercises/additional_exercise9.py
+++ 1 - Variables/Additional Exercises/additional_exercise9.py
@@ -6,13 +6,14 @@ import math
 
 print("Circular Swimming Pool")
 print("This program calculates the volume of a circular swimming pool")
-print()
-diameter = float(input("Please enter the diameter of the pool: "))
-depth = float(input("Please enter the depth of the pool: "))
-radius = diameter/2
+
+math():
+diameter = input("10")
+depth = input("15")
+radius = ("diameter/2")
 #radius**2 means radius squared
-area = round(math.pi * radius**2,2)
-volume = round(area * depth,2)
-print()
-print("The volume of the swimming pool is {0}.".format(volume))
+area = round("math.pi * radius**2,2")
+volume = round("area * depth,2")
+circular swimming pool():
+print("The volume of the swimming pool is {1178,097245}.".(volume))
 
