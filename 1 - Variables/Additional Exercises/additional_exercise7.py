8990e6fd9da04043b93bb5fdbe086b7b80acabf5
mosesdeveloper-cyber <norteymoses769@gmail.com>
2025-11-25 23:24:32 +0100

    Update additional_exercise7.py
    8i

--- 1 - Variables/Additional Exercises/additional_exercise7.py
+++ 1 - Variables/Additional Exercises/additional_exercise7.py
@@ -2,29 +2,24 @@
 #additional exercise 7
 #sample solution
 
-print("Foreign Currency")
-print("This program asks for your holiday money (in pounds)")
-print("and the current exchange rate then displays the number of")
-print("euros you will recieve.")
-print()
-holidayMoney = int(input("Please enter amount of money you will take on holiday: "))
-exchangeRate = float(input("Please enter the current pound-euro exchange rate: "))
-print()
-#this is a div calculation
-euroHolidayMoney = holidayMoney//exchangeRate
-print("You will receive {0} Euros for your holiday.".format(euroHolidayMoney))
-print("This will be made up of:")
-euro50s = int(euroHolidayMoney // 50)
-#this is a mod calculation
-remainingEuros = euroHolidayMoney % 50
-euro20s = int(remainingEuros // 20)
-remainingEuros = remainingEuros % 20
-euro10s = int(remainingEuros // 10)
-remainingEuros = remainingEuros % 10
-euro5s = int(remainingEuros // 5)
-remainingEuros = remainingEuros % 5
-print("{0} 50 Euro notes.".format(euro50s))
-print("{0} 20 Euro notes.".format(euro20s))
-print("{0} 10 Euro notes.".format(euro10s))
-print("{0} 5 Euro notes.".format(euro5s))
-print("and {0} in coins.".format(remainingEuros))
+
+
+currency():
+holidayMoney = input("20pounds")
+exchangeRate = input("22,78€")
+money():
+euroHolidayMoney = ("holidayMoney/exchangeRate")
+print("You will receive {0,8779631255} Euros for your holiday.".(euroHolidayMoney))
+euro50s = int("euroHolidayMoney/50")
+remainingEuros = ("euroHolidayMoney % 50")
+euro20s = int("remainingEuros / 20")
+remainingEuros = ("remainingEuros % 20")
+euro10s = int("remainingEuros / 10")
+remainingEuros = ("remainingEuros % 10")
+euro5s = int("remainingEuros / 5")
+remainingEuros = ("remainingEuros % 5")
+print("{0,01755926251} 50 Euro notes.".(euro50s))
+print("{0,02194907814} 20 Euro notes.".(euro20s))
+print("{0,04387504388} 10 Euro notes.".(euro10s))
+print("{0,08775008776} 5 Euro notes.".(euro5s))
+print("and {0,02193752194} in coins.".(remainingEuros))
