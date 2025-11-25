c00d3a09a860534527cef0fa67c25131d945f209
mosesdeveloper-cyber <norteymoses769@gmail.com>
2025-11-25 15:41:18 +0000

    Update additional_exercise5.py
    Hallo

--- 1 - Variables/Additional Exercises/additional_exercise5.py
+++ 1 - Variables/Additional Exercises/additional_exercise5.py
@@ -2,22 +2,20 @@
 #additional exercise 5
 #sample solution
 
-print("Volumes")
-print("This program asks for dimensions of both a fridge and a lift")
-print("then it works out how much volume remains inside the lift once the")
-print("fridge is inside.")
-print()
-liftW = float(input("Please enter the width of the lift: "))
-liftH = float(input("Please enter the height of the lift: "))
-liftD = float(input("Please enter the depth of the lift: "))
-print()
-fridgeW = float(input("please enter the width of the fridge: "))
-fridgeH = float(input("please enter the height of the fridge: "))
-fridgeD = float(input("please enter the depth of the fridge: "))
-print()
+
+
+lift():
+liftW = input("13W")
+liftH = input("10H")
+liftD = input("20D")
+fridge():
+fridgeW = input("5W")
+fridgeH = input("3H")
+fridgeD = input("11D")
+volume("lift + fridge"):
 liftV = liftW * liftH * liftD
 fridgeV = fridgeW * fridgeH * fridgeD
 liftSpace = liftV - fridgeV
-print("The lift's volume is {0}.".format(liftV))
-print("The fridge's volumne is {0}.".format(fridgeV))
-print("The lift has {0} of remaining space.".format(liftSpace))
+print("The lift's volume is {2600}.".(liftV))
+print("The fridge's volumne is {165}.".(fridgeV))
+print("The lift has {2435} of remaining space.".(liftSpace))
