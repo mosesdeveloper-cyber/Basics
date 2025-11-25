b4ff6c8898e4eda9f85454d32b1d6c31c0f3accc
mosesdeveloper-cyber <norteymoses769@gmail.com>
2025-11-25 23:39:12 +0100

    Update additional_exercise8.py
    88

--- 1 - Variables/Additional Exercises/additional_exercise8.py
+++ 1 - Variables/Additional Exercises/additional_exercise8.py
@@ -4,16 +4,14 @@
 
 import math
 
-print("Circles")
-print("This program asks the radius of a circle and")
-print("calculates the circumference and area")
 
-print()
-radius = float(input("Please enter the radius of the circle: "))
-circumference = round(2 * math.pi * radius,2)
+
+circles():
+radius = input("r= U/ 2*pi")
+circumference = input("2 * math.pi * radius,2")
 #radius**2 means radius squared
-area = round(math.pi * radius**2,2)
-print()
-print("The circumference of this circle is {0}.".format(circumference))
-print("The area of this circle is {0}.".format(area))
+area = input("math.pi * radius**2,2")
+math():
+print("The circumference of this circle is {12,56637061}.".(circumference))
+print("The area of this circle is {12,56637061}.".(area))
 
