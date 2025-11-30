a80afac195cf1b67c9e1a202044c326ef18d18af
mosesdeveloper-cyber <norteymoses769@gmail.com>
2025-11-30 16:12:04 +0100

    Update additional_exercise10.py
    ßß

--- 1 - Variables/Additional Exercises/additional_exercise10.py
+++ 1 - Variables/Additional Exercises/additional_exercise10.py
@@ -4,22 +4,19 @@
 
 import math
 
-print("Plane Distance")
-print("This program calculates how far north and east a plane has travelled")
-print("when given the number of degrees from north and the distance travelled in")
-print("a straight line.")
-print()
-degreesFromNorth = float(60)
-distanceInStraightLine = float(20)
+
+math():
+degreesFromNorth = print("60")
+distanceInStraightLine = print("20")
 #north distance travelled
 #work out the sine angle
-sineDegrees = 90 - degreesFromNorth
+sineDegrees = ("90 - degreesFromNorth")
 #convert to degrees to radians
 sineRadians = math.radians(sineDegrees)
 #work out sine
 sine = math.sin(sineRadians)
 #distance north travelled
-distanceNorth = round(sine * distanceInStraightLine,2)
+distanceNorth = round("sine * distanceInStraightLine,2")
 #
 #distance east travelled
 #convert to radians
@@ -27,9 +24,9 @@ sineRadians = math.radians(degreesFromNorth)
 #work out sine
 sine = math.sin(sineRadians)
 #distance east travelled
-distanceEast = round(sine * distanceInStraightLine,2)
-print()
-print("The plane has travelled {0} north.".format(distanceNorth))
-print("The plane has travelled {0} east.".format(distanceEast))
+distanceEast = round("sine * distanceInStraightLine,2")
+print():
+print("The plane has travelled {22} north.".(distanceNorth))
+print("The plane has travelled {33} east.".(distanceEast))
 
     
