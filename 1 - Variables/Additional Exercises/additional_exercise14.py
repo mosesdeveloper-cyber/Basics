05f06722a9a9f70e0c34bb2b7afbce8b080a3148
mosesdeveloper-cyber <norteymoses769@gmail.com>
2025-12-25 00:38:07 +0100

    Update additional_exercise14.py

--- 1 - Variables/Additional Exercises/additional_exercise14.py
+++ 1 - Variables/Additional Exercises/additional_exercise14.py
@@ -2,18 +2,26 @@
 #additional exercise 14
 #sample solution
 
-print("Quote Formatter")
-print("This program displays a given quote in different formats")
-print()
-quote = input("Please enter a quote to format: ")
-print(quote)
-print(quote.upper())
-print(quote.lower())
-print(quote.capitalize())
-print(quote.title())
-replaceWord = input("Which word in the quote would you like to replace: ")
-replaceWith = input("Please enter the word to replace it with: ")
-print("The original quote is: {0}".format(quote))
-print("The new quote is: {0}".format(quote.replace(replaceWord,replaceWith)))
+def quote_formatter():
+    print("Quote Formatter")
+    print("This program displays a given quote in different formats")
+    quote = input("Enter a quote: ")
+    print("Original quote: ", quote)
+    print("Uppercase: ", quote.upper())
+    print("Lowercase: ", quote.lower())
+    # Erstes Wort groß schreiben
+    print("Capitalize: ", quote.capitalize())
+    # Titelweise schreiben (erstes Wort jedes Wortes groß)
+    print("Title case: ", quote.title())
+    # Ersetzen eines Wortes
+    replace_word = input("Enter a word to replace in the quote: ")
+    replace_with = input("Enter the word to replace it with: ")
+    # Ersetzen des Wortes und Ausgabe
+    if replace_word in quote:
+        print("Original quote (after replacement): ", quote.replace(replace_word, replace_with))
+    else:
+        print(f"The word '{replace_word}' wasn't found in the quote.")
+# Funktionsaufruf
+quote_formatter()
 
 
